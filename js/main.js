befe6bf05166e9a2c25f39947a3542d61dce839e
Luis Carlos Salazar Pacheco <ing.lcsalazar@hotmail.com>
2015-02-27 16:04:06 -0500

    p

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	
 	$(".dropdown-button").dropdown();
 	$(".button-collapse").sideNav();
+	$(".slider").slider({
+		indicators: false,
+		interval: 3000//milesima de segundos
+	});
 });
